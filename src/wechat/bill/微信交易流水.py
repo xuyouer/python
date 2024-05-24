78c05e637dc8c638a86c6681e446e3f48111c85b
xuyouer <81155010+xuyouer@users.noreply.github.com>
2024-05-24 20:27:51 +0800

    更新 微信交易流水.py

--- src/wechat/bill/微信交易流水.py
+++ src/wechat/bill/微信交易流水.py
@@ -11,6 +11,10 @@ import re
 import time
 from concurrent.futures import ThreadPoolExecutor
 
+import tkinter as tk
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
+
 import pandas as pd
 
 # 充值/提现/理财通购买/零钱通存取/信用卡还款等交易，将计入中性交易
@@ -125,6 +129,8 @@ def parse_csv_files_multithreaded(directory_path):
         executor.map(parse_csv_file, files)
     print("CSV文件读取 OK")
 
+    log_message("CSV文件读取 OK")
+
 
 # 去重
 def deduplicate_transactions(transactions):
@@ -135,14 +141,14 @@ def deduplicate_transactions(transactions):
 
 
 # 将交易记录保存到CSV文件
-def save_transactions_to_csv(transactions, output_file):
+def save_transactions_to_csv(transactions, output_dir):
     """
     将交易记录列表保存到指定的CSV文件中
 
     :param transactions: 交易记录的列表
-    :param output_file: 输出的CSV文件路径
+    :param output_dir: 输出的CSV文件路径
     """
-    with open(output_file, mode='w', newline='', encoding='utf-8-sig') as csvfile:
+    with open(output_dir, mode='w', newline='', encoding='utf-8-sig') as csvfile:
         fieldnames = ['交易时间', '交易类型', '交易对方', '商品', '收支类型',
                       '金额(元)', '支付方式', '当前状态', '交易单号', '商户单号', '备注']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -150,25 +156,29 @@ def save_transactions_to_csv(transactions, output_file):
         writer.writeheader()
         for transaction in transactions:
             writer.writerow(transaction)
-    print(f"交易记录 '{output_file}' OK")
+    print(f"交易记录 '{output_dir}' OK")
+
+    log_message(f"交易记录 '{output_dir}' OK")
 
 
 # 将交易记录保存到Excel文件
-def save_transactions_to_excel(transactions, output_file):
+def save_transactions_to_excel(transactions, output_dir):
     """
     将交易记录列表保存到指定的Excel文件中
 
     :param transactions: 交易记录的列表
-    :param output_file: 输出的Excel文件路径
+    :param output_dir: 输出的Excel文件路径
     """
     df = pd.DataFrame(transactions)
-    df.to_excel(output_file, index=False, engine='openpyxl')
-    print(f"交易记录 '{output_file}' OK")
+    df.to_excel(output_dir, index=False, engine='openpyxl')
+    print(f"交易记录 '{output_dir}' OK")
+
+    log_message(f"交易记录 '{output_dir}' OK")
 
 
 # 根据微信昵称创建目录并返回目录路径
-def create_wechat_directory(nickname):
-    directory_path = os.path.join('.', nickname)
+def create_wechat_directory(nickname, output_dir='./'):
+    directory_path = os.path.join(output_dir, nickname)
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
     return directory_path
@@ -198,15 +208,17 @@ def write_data_to_txt(nickname, data, directory_path):
 
 
 # 在解析完所有文件后, 对交易明细列表进行去重, 执行后续操作
-def post_process_all_files():
+def post_process_all_files(output_dir='./'):
     global transactions
     transactions = deduplicate_transactions(transactions)
     print("交易记录去重 OK")
 
+    log_message("交易记录去重 OK")
+
     nickname = data['微信昵称']
 
     # 写入统计信息
-    directory_path = create_wechat_directory(nickname)
+    directory_path = create_wechat_directory(nickname, output_dir)
     write_data_to_txt(nickname, data, directory_path)
 
     # 写入csv文件
@@ -217,7 +229,12 @@ def post_process_all_files():
     nickname_output_excel = os.path.join(directory_path, f"{nickname}_交易流水.xlsx")
     save_transactions_to_excel(transactions, nickname_output_excel)
 
+    # 重置统计数据
+    reset_data()
+
     print(f"'{nickname}' 的数据 OK")
+
+    log_message(f"'{nickname}' 的数据 OK")
 
 
 # 倒计时
@@ -229,13 +246,24 @@ def countdown(seconds):
         time.sleep(1)
 
 
+# 重置统计数据
+def reset_data():
+    data['总笔数'] = 0
+    data['收入']['笔数'] = 0
+    data['收入']['总额'] = 0.0
+    data['支出']['笔数'] = 0
+    data['支出']['总额'] = 0.0
+    data['中性交易']['笔数'] = 0
+    data['中性交易']['总额'] = 0.0
+
+
 def test():
     # CSV文件路径
     # csv_file_path = '微信支付账单(20230701-20231001).csv'
     csv_file_paths = './'
     csv_file_paths = input("请输入CSV文件所在目录的路径（直接回车默认为当前目录）: ")
     csv_file_paths = csv_file_paths if csv_file_paths else './'
-    print(f"正在处理目录 '{csv_file_paths}' 下的CSV文件...")
+    print(f"开始处理目录 '{csv_file_paths}' 下的CSV文件...")
 
     # 调用函数解析文件
     # parse_csv_file(csv_file_path)
@@ -261,6 +289,158 @@ def test():
     3. 点击确认
 """
 
+
+# if __name__ == '__main__':
+#     test()
+#     countdown(5)
+
+
+# 退出应用程序
+def exit_program():
+    root.destroy()
+
+
+# 获取用户选择的输入目录
+def on_select_input_directory():
+    global input_dir
+    input_dir = filedialog.askdirectory()
+    if input_dir:
+        log_message(f"输入目录: {input_dir}")
+    else:
+        input_dir = "./"
+        log_message("输入目录: 未选择, 默认使用当前目录")
+
+
+# 获取用户选择的输出目录
+def on_select_output_directory():
+    global output_dir
+    output_dir = filedialog.askdirectory()
+    if output_dir:
+        log_message(f"输出目录: {output_dir}")
+    else:
+        output_dir = "./"
+        log_message("输出目录: 未选择, 默认使用当前目录")
+
+
+# 传入用户选择的目录
+def process_selected_directory(input_dir='./', output_dir='./'):
+    csv_file_paths = input_dir
+    log_message(f"开始处理目录 '{csv_file_paths}' 下的CSV文件...")
+    # 多线程读取CSV文件
+    parse_csv_files_multithreaded(csv_file_paths)
+    # 后续处理
+    post_process_all_files(output_dir)
+    log_message("处理完成\n")
+
+
+# 开始处理
+def process():
+    process_selected_directory(input_dir, output_dir)
+
+
+# 日志打印
+def log_message(message):
+    log.insert(tk.END, message + "\n")  # 在ScrolledText的末尾插入新消息
+    log.see(tk.END)  # 滚动到底部
+    root.update_idletasks()  # 更新UI
+
+
+# 窗口大小和位置设置
+def window_setup(root, width=500, height=550):
+    window_width = width
+    window_height = height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    center_x = int(screen_width / 2 - window_width / 2)
+    center_y = int(screen_height / 2 - window_height / 2)
+    root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+
+# 设置图标
+def set_window_icon(root, icon_path):
+    if os.path.exists(icon_path):
+        root.iconbitmap(icon_path)
+
+
+# 选择目录
+def create_directory_selector(root, label_text, cmd_function, button_text):
+    frame = tk.Frame(root)
+    frame.pack(fill=tk.X, padx=20, pady=(20, 10))
+    tk.Label(frame, text=label_text, font=("宋体", 12)).pack(side=tk.LEFT)
+    tk.Button(frame, text=button_text,
+              command=lambda: cmd_function(),
+              font=("宋体", 12),
+              borderwidth=1,
+              relief='solid',
+              padx=5,
+              pady=2).pack(side=tk.LEFT, padx=5)
+
+
+# 日志区域
+def setup_log_area(root):
+    log_frame = tk.Frame(root)
+    log_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+    global log
+    log = ScrolledText(log_frame, height=10)
+    log.pack(fill=tk.BOTH, expand=True)
+
+
+# 默认日志
+def log_initial_message():
+    log_message("默认输入目录: 当前目录")
+    log_message("默认输出目录: 当前目录\n")
+
+
+def main_gui():
+    global root, log, input_dir, output_dir
+
+    # 初始化目录
+    input_dir = "./"
+    output_dir = "./"
+
+    # 初始化Tkinter应用
+    root = tk.Tk()
+    root.title("微信交易记录分析工具")
+
+    # 设置窗口大小
+    window_setup(root)
+
+    # 设置窗口图标
+    set_window_icon(root, "favicon.ico")
+
+    # 输入目录选择
+    create_directory_selector(root, "选择CSV文件所在目录:", on_select_input_directory, "浏览")
+
+    # 输出目录选择
+    create_directory_selector(root, "选择输出目录:", on_select_output_directory, "浏览")
+
+    # 日志展示区域
+    setup_log_area(root)
+
+    # 显示默认目录的日志信息
+    log_initial_message()
+
+    # 处理按钮
+    tk.Button(root, text="开始处理",
+              command=process,
+              font=("宋体", 12),
+              borderwidth=1,
+              relief='solid',
+              padx=5,
+              pady=2).pack(side=tk.LEFT, padx=20, pady=20)
+
+    # 退出按钮
+    tk.Button(root, text="退出",
+              command=exit_program,
+              font=("宋体", 12),
+              borderwidth=1,
+              relief='solid',
+              padx=5,
+              pady=2).pack(side=tk.RIGHT, padx=20, pady=20)
+
+    # 循环
+    root.mainloop()
+
+
 if __name__ == '__main__':
-    test()
-    countdown(5)
+    main_gui()
